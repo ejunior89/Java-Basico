5d2c6285781d290c9df20dc323c474cb18de8ab8
ejunior89 <eliomaiajunior@gmail.com>
2025-07-11 11:03:16 -0400

    feat: If-Else

---
+++ src/ExemploIf.java
@@ -0,0 +1,10 @@
+public class ExemploIf {
+  public static void main(String[] args) {
+    int idade = 20;
+
+    if(idade >= 18) {
+      System.out.println("Voce é maior de idade, " + "Tem " + idade + " anos.");
+    }
+    System.out.println("Fim do programa.");
+  }
+}
---
+++ src/ExemploIfElse.java
@@ -0,0 +1,11 @@
+public class ExemploIfElse {
+  public static void main(String[] args) {
+    double nota = 8.6;
+
+    if (nota >= 5.0) {
+      System.out.println("Aprovado com nota: " + nota);
+    } else {
+      System.out.println("Reprovado com nota: " + nota);
+    }
+  }
+}
