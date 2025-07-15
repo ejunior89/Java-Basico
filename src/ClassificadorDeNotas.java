e3b91e5c9d42d6e6db9493cab496cd36958fa17f
ejunior89 <eliomaiajunior@gmail.com>
2025-07-15 17:37:42 -0400

    ajustes

--- src/ClassificadorDeNotas.java
+++ src/ClassificadorDeNotas.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ClassificadorDeNotas {
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a nota do aluno (0 a 100): ");
+
+        int nota = scanner.nextInt();
+
+        if (nota < 0 || nota > 100) {
+            System.out.println("Nota inválida. Por favor, insira uma nota entre 0 e 100.");
+        } else if (nota >= 90) {
+            System.out.println("A");
+        } else if (nota >= 80) {
+            System.out.println("B");
+        } else if (nota >= 70) {
+            System.out.println("C");
+        } else if (nota >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+        scanner.close();
+    }
 }
