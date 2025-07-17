bd2ab0b9cdd763f16058179a4e0f2db6dbf04fb9
ejunior89 <eliomaiajunior@gmail.com>
2025-07-17 13:30:43 -0400

    feat: laço for

---
+++ src/ContadorFor.java
@@ -0,0 +1,8 @@
+public class ContadorFor {
+  public static void main(String[] args) {
+    System.out.println("Contador de numeros pares: ");
+    for (int i = 2; i <= 10; i +=2) {
+      System.out.println(i);
+    }
+  }
+}
